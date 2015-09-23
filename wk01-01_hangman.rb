7b32fed07614cd91ab464ca81a17e8c53aba46cd
violetaria <terri.chu@outlook.com>
2015-09-23 12:10:24 -0400

    removed turn_count, moved answers and guesses to sets

--- wk01-01_hangman.rb
+++ wk01-01_hangman.rb
@@ -6,7 +6,7 @@ word_list = [
     "coffee", "totalitarianism", "metacircular", "interpreter",
     "wednesday", "ruby", "evaluation", "consternation", "chicanery"
 ]
-turn_count = 6
+MAX_TURNS = 6
 answer = word_list.sample
 
 def greeting
@@ -17,50 +17,64 @@ def greeting
   puts "\n\n"
 end
 
-def game_over?(turn_count,answer,guesses)
-  win?(answer,guesses) || turn_count.zero? # turn_count==0
+def game_over?(answer,guesses)
+  win?(answer,guesses) || turns_left(answer,guesses).zero? # turn_count==0
 end
 
 def win?(answer,guesses)
-  answer.chars.all? {|char| guesses.include?(char)}
-  # winner = true
+  answer_set = answer.chars.to_set
+  guesses >= answer_set
+
+  # attempt 4
+  # guesses.superset? answer_set
+
+  # attempt 3 below
+  # answer_set.subset? guesses
+
+  # attempt 2 below
+  # answer.chars.all? {|char| guesses.include?(char)}
+
+
+  # attempt 1 below
+  # # winner = true
   # answer.chars.each.do |letter|
   #   winner = false unless guesses.include?(letter)
   # end
   # winner
 end
 
-def get_letter()
+def get_letter
   puts
   puts "Please guess a letter: "
   gets.chomp.downcase
 end
 
-def valid_guess?(guesses, letter)
+def valid_guess?(letter)
   valid_letters = ("a".."z").to_a
   if(!valid_letters.include?(letter))
     puts "The guess it not a single letter between a and z!  Try again."
     false
-  elsif(guesses.include?(letter))
-    puts "You already guessed #{letter}! Try again!"
-    false
+  # elsif(guesses.include?(letter))
+  #   puts "You already guessed #{letter}! Try again!"
+  #   false
   else
     true
   end
 end
 
-def prompt_player(guesses)
+def prompt_player
   #binding.pry
   guess = get_letter
-  until valid_guess?(guesses,guess)
+  until valid_guess?(guess)
     guess = get_letter
   end
   guess
 end
 
-def show_progress(partial,turn_count)
+def show_progress(partial,answer,guesses)
+  turns_remaining = turns_left(answer, guesses)
   puts "The current word is: #{partial}."
-  puts "You have #{turn_count} guesses left."
+  puts "You have #{turns_remaining} guesses left."
 end
 
 def make_partial(guesses,answer)
@@ -73,10 +87,10 @@ def make_partial(guesses,answer)
   end
 end
 
-def take_turn(guesses,answer,turn_count)
+def take_turn(guesses,answer)
   partial = make_partial(guesses,answer).join
-  show_progress(partial,turn_count)
-  prompt_player(guesses)
+  show_progress(partial,answer,guesses)
+  prompt_player
 end
 
 def postmortem(answer, guesses)
@@ -96,23 +110,33 @@ def play_again?
   replay == "y"
 end
 
-def play_hangman(words,turns)
+def play_hangman(words)
   answer = words.sample
-  hangman(answer,turns)
+  hangman(answer)
   while play_again?
-    hangman(answer,turns)
+    hangman(answer)
   end
 end
 
-def hangman(answer,turn_count)
+def turns_left(answer,guesses)
+  answer_set = answer.chars.to_set
+  wrong_guesses = (guesses - answer_set).count
+
+  # attempt 1
+  # wrong_guesses = guesses.difference(answer_set).count
+
+  #binding.pry
+  MAX_TURNS - wrong_guesses
+end
+
+def hangman(answer)
   guesses = Set.new
   greeting
-  until game_over?(turn_count,answer,guesses)
-    guess = take_turn(guesses,answer,turn_count).downcase
+  until game_over?(answer,guesses)
+    guess = take_turn(guesses,answer).downcase
     guesses.add(guess) # accounting for upper vs. lowercase, all of our pre-set words are lowercase
-    turn_count-=1 unless answer.include?(guess)
   end
   postmortem(answer,guesses)
 end
 
-play_hangman(word_list,turn_count)
+play_hangman(word_list)
