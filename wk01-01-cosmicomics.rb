abc96953b04580666bd38961eddf57eb06b191e6
violetaria <terri.chu@outlook.com>
2015-09-24 11:58:42 -0400

    Updated for performance

--- wk01-01-cosmicomics.rb
+++ wk01-01-cosmicomics.rb
@@ -16,4 +16,26 @@ def join_words(word1,word2)
   word1 + word2[1..-1]
 end
 
+def build_dictionary(file)
+  dict = Hash.new([]) # new hash, empty
+  file.each_line do |line| # loop over each line inf file
+    word = line.chomp # chomp each word
+    if word.length>3  #only if length > 3
+      dict[word.length] += [word]  # add to hash under that key
+    end
+  end
+  dict
+end
+
+def find_funny_words(filepath)
+  # putting the file.open into a do block, will automtically close the file when it leaves the block
+  File.open(filepath, 'r') do |file|
+    word_groups = build_dictionary(file)
+    word_groups.each do |length, words|
+      words.combinations(2).each |word1, word2|
+        puts join_words(word1,word2) if funny_word?(word1,word2)
+    end
+  end
+end
+
 binding.pry
